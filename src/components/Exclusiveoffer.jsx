cee6975f97bf451016d89a801fb33158fee45bfe
Asrar Ali <mohdasrar3825@gmail.com>
2025-05-28 13:32:04 +0530

    title fix

--- src/components/Exclusiveoffer.jsx
+++ src/components/Exclusiveoffer.jsx
@@ -46,7 +46,7 @@ function Exclusiveoffer() {
               </div>
 
               {/* Bottom Content */}
-              <div className="space-y-4">
+              <div className="space-y-4"> 
                 {/* Expiry Date */}
                 <div className="flex items-center gap-2 text-white/80">
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -55,7 +55,7 @@ function Exclusiveoffer() {
                   <span className="text-sm">Expires {item.expiryDate}</span>
                 </div>
 
-                {/* CTA Button */}
+             
               
               </div>
             </div>
